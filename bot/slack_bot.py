e6fe0afd2c3bb5d05c9722c3976e2f5051b8aca0
Kenneth Myers <kennmyers@users.noreply.github.com>
2016-08-31 20:34:30 -0400

    5

--- bot/slack_bot.py
+++ bot/slack_bot.py
@@ -71,7 +71,7 @@ class SlackBot(object):
                     link = data["data"]["children"][1]["data"]["url"]
                     
                     if self.prev_urls[i] != link:
-                        msg_writer.send_message('#bot_test', link[-5:])
+                        msg_writer.send_message('#bot_test', "sup dawg")
                         self.prev_urls[i] = link
                     time.sleep(2)
                 
